5ef0ad005d6e63f6e5861421f9f86fbeabefaf32
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-02 18:26:46 -0500

    ast test

--- src/pages/api/begin.js
+++ src/pages/api/begin.js
@@ -1,16 +1,6 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-      data = req.body
-      // if (data["acc"] === undefined) {
-      //   res.status(400).json({text: "Oof, seems like you didn't include 'acc' as your 'key'"})
-      // }
-      // else if (data["acc"].toLowerCase() === "aggie coding club") {
-      //   res.status(200).json({text:"\nThat's Correct!\n\nFor the next question, make a POST request to https://acc-api-scavenger-hunt.vercel.app/api/numofficers with a JSON object of key “num_officers” and the value as an integer number as your answer to how many officers ACC has. Go to https://aggiecodingclub.com to find out!"})
-      // }
-      // else if (data["acc"].toLowerCase() !== "aggie coding club") {
-      //   res.status(400).json({text:"\nHmmm that's not the correct 'value'\n\nDouble check that you have the acroynm correct."})
-      // }
-      res.status(200).json({text: "test"})
+      res.status(200).json({text: '\nwefwef'})
     }
     else if (req.method === 'GET') {
       res.status(200).json({ text: '\nWelcome to the ACC API Scavenger Hunt!!!\n\nStart by creating a POST request on the same URL.\n\nInclude a JSON object with ACC\'s full name. Ex: {"acc", "your_answer_here"}' });
