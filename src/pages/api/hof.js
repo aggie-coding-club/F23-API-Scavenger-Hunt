de139877a59bcac558b8c6d5e0fbb280187e03a0
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-18 17:21:32 -0500

    hof change

--- src/pages/api/hof.js
+++ src/pages/api/hof.js
@@ -1,4 +1,4 @@
-import {Timestamp, doc, setDoc} from "firebase/firestore";
+import {serverTimestamp, doc, setDoc} from "firebase/firestore";
 import { db } from "@/firebase";
 
 export default async function handler(req, res) {
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       const data = req.body
       if (Number(data["key"]) === Number(process.env.COMPLETION_KEY)) {
         await setDoc(doc(db, "halloffame", data["name"]), {
-            "time": Timestamp
+            timestamp: serverTimestamp()
         })
         res.status(200).send("\nYour name should now be on the leaderboard! If you have time, please answer some feedback questions!\n\nSend a POST request to\nhttps://acc-api-scavenger-hunt.vercel.app/api/feedback/enjoy \nwith your answer to the question:\n'did you enjoy the workshop?'\nex: {'feedback':'your_feedback'}\n")
       }
