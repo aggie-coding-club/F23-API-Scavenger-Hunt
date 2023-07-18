555267b0c6e770014cfb78df73d1d9b917750c19
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-18 17:12:39 -0500

    test 2

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -23,7 +23,7 @@ export default async function handler (req, res) {
           res.status(400).send("eww ur not human~~")
         }
       } catch(e) {
-        res.status(400).send("error occurred", e)
+        res.status(400).send("error occurred")
       }
     }
 
